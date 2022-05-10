7b1fe355ff287253f2d026ef4099e99cb06739c7
4ZSan <za_no@hotmail.fr>
2022-05-10 16:58:12 +0200

    Test js

--- js.js
+++ js.js
@@ -1,7 +1,30 @@
-alert("aa");
-var contentFrame = 888;
-alert(contentFrame);
-window.scroll(0, contentFrame);
+/*var position = window.scrollY;
+var count = 0;
 
+function posY(){
+  alert(window.scrollY);
+  alert(window.scrollTop);
+}
 
-window.addEventListener(scroll,window.scroll(0, contentFrame));
+function throttle(callback, delay) {
+    var last;
+    var timer;
+    return function () {
+        var context = this;
+        var now = +new Date();
+        var args = arguments;
+        if (last && now < last + delay) {
+            // le délai n'est pas écoulé on reset le timer
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                last = now;
+                callback.apply(context, args);
+            }, delay);
+        } else {
+            last = now;
+            callback.apply(context, args);
+        }
+    };
+}
+
+window.addEventListener('scroll', throttle((posY),1000));*/
