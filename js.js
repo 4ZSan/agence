30c8444e573522486c50ffff8b8877a23de2f157
4ZSan <za_no@hotmail.fr>
2022-05-19 14:00:27 +0200

    Nouveau fonctionnement du scroll js

--- js.js
+++ js.js
@@ -1,56 +1,110 @@
-var initial_position = 0;
+(function() {
+	"use strict";
+	/*[pan and well CSS scrolls]*/
+	var pnls = document.querySelectorAll('.panel').length,
+		scdir, hold = false;
 
-function posY(){
-  var current_position = window.scrollY;
-  alert("position initial : "+initial_position);
-  alert("position actuel : "+current_position);
-  if (current_position > initial_position) {
-    initial_position = initial_position + 888;
-    window.scroll(0,initial_position);
-  }
-  if (current_position < initial_position) {
-    initial_position = initial_position - 888;
-    window.scroll(0,initial_position);
-  }
-}
+	function _scrollY(obj) {
+		var slength, plength, pan, step = 100,
+			vh = window.innerHeight / 100,
+			vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
+		if ((this !== undefined && this.id === 'well') || (obj !== undefined && obj.id === 'well')) {
+			pan = this || obj;
+			plength = parseInt(pan.offsetHeight / vh);
+		}
+		if (pan === undefined) {
+			return;
+		}
+		plength = plength || parseInt(pan.offsetHeight / vmin);
+		slength = parseInt(pan.style.transform.replace('translateY(', ''));
+		if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
+			slength = slength - step;
+		} else if (scdir === 'down' && slength < 0) {
+			slength = slength + step;
+		} else if (scdir === 'top') {
+			slength = 0;
+		}
+		if (hold === false) {
+			hold = true;
+			pan.style.transform = 'translateY(' + slength + 'vh)';
+			setTimeout(function() {
+				hold = false;
+			}, 1000);
+		}
+		console.log(scdir + ':' + slength + ':' + plength + ':' + (plength - plength / pnls));
+	}
+	/*[swipe detection on touchscreen devices]*/
+	function _swipe(obj) {
+		var swdir,
+			sX,
+			sY,
+			dX,
+			dY,
+			threshold = 100,
+			/*[min distance traveled to be considered swipe]*/
+			slack = 50,
+			/*[max distance allowed at the same time in perpendicular direction]*/
+			alT = 500,
+			/*[max time allowed to travel that distance]*/
+			elT, /*[elapsed time]*/
+			stT; /*[start time]*/
+		obj.addEventListener('touchstart', function(e) {
+			var tchs = e.changedTouches[0];
+			swdir = 'none';
+			sX = tchs.pageX;
+			sY = tchs.pageY;
+			stT = new Date().getTime();
+			//e.preventDefault();
+		}, false);
 
-function throttle(callback, delay) {
-    var last;
-    var timer;
-    return function () {
-        var context = this;
-        var now = +new Date();
-        var args = arguments;
-        if (last && now < last + delay) {
-            // le délai n'est pas écoulé on reset le timer
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                last = now;
-                callback.apply(context, args);
-            }, delay);
-        } else {
-            last = now;
-            callback.apply(context, args);
-        }
-    };
-}
+		obj.addEventListener('touchmove', function(e) {
+			e.preventDefault(); /*[prevent scrolling when inside DIV]*/
+		}, false);
 
-function debounce(func, wait, immediate, context) {
-    var result;
-    var timeout = null;
-    return function() {
-        var ctx = context || this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) result = func.apply(ctx, args);
-        };
-        var callNow = immediate && !timeout;
-        // Tant que la fonction est appelée, on reset le timeout.
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) result = func.apply(ctx, args);
-        return result;
-    };
-}
+		obj.addEventListener('touchend', function(e) {
+			var tchs = e.changedTouches[0];
+			dX = tchs.pageX - sX;
+			dY = tchs.pageY - sY;
+			elT = new Date().getTime() - stT;
+			if (elT <= alT) {
+				if (Math.abs(dX) >= threshold && Math.abs(dY) <= slack) {
+					swdir = (dX < 0) ? 'left' : 'right';
+				} else if (Math.abs(dY) >= threshold && Math.abs(dX) <= slack) {
+					swdir = (dY < 0) ? 'up' : 'down';
+				}
+				if (obj.id === 'well') {
+					if (swdir === 'up') {
+						scdir = swdir;
+						_scrollY(obj);
+					} else if (swdir === 'down' && obj.style.transform !== 'translateY(0)') {
+						scdir = swdir;
+						_scrollY(obj);
 
-window.addEventListener('scroll', debounce((posY),1000));
+					}
+					e.stopPropagation();
+				}
+			}
+		}, false);
+	}
+	/*[assignments]*/
+	var well = document.getElementById('well');
+	well.style.transform = 'translateY(0)';
+	well.addEventListener('wheel', function(e) {
+		if (e.deltaY < 0) {
+			scdir = 'down';
+		}
+		if (e.deltaY > 0) {
+			scdir = 'up';
+		}
+		e.stopPropagation();
+	});
+	well.addEventListener('wheel', _scrollY);
+	_swipe(well);
+	var tops = document.querySelectorAll('.top');
+	for (var i = 0; i < tops.length; i++) {
+		tops[i].addEventListener('click', function() {
+			scdir = 'top';
+			_scrollY(well);
+		});
+	}
+})();
